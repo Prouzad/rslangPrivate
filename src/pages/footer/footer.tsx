73cbe6758d8f78b5b69b05828ac94733c14ba6f3
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2022-08-27 14:55:14 +0500

    refactor: fix footer alignment

--- src/pages/footer/footer.tsx
+++ src/pages/footer/footer.tsx
@@ -1,30 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import AvatarImg from '../../assets/icon/avatar.png';
 import RsLogo from '../../assets/icon/rs_school_js.svg';
 
 const Footer = () => {
 	return (
 		<footer className='footer'>
-			<div className="footerContainer">
-				<div className="logoRs">
+			<div className='footerContainer'>
+				<div className='logoRs'>
 					<Link to='https://rs.school/js/'>
-						<img src={RsLogo} alt="" />
+						<img src={RsLogo} alt='' />
 					</Link>
 				</div>
-				<div className="githubLinks">
+				<div className='githubLinks'>
 					<Link to='/'>
-						<img src={AvatarImg} alt="github"/>
+						<img src={AvatarImg} alt='github' />
 					</Link>
 					<Link to='/'>
-						<img src={AvatarImg} alt="github"/>
+						<img src={AvatarImg} alt='github' />
 					</Link>
 					<Link to='/'>
-						<img src={AvatarImg} alt="github"/>
+						<img src={AvatarImg} alt='github' />
 					</Link>
 				</div>
-				<div className="yearApp">
-					2022
-				</div>
+				<div className='yearApp'>2022</div>
 			</div>
 		</footer>
 	);
