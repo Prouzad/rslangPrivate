8677d8cdd4ec0fef136cc666b0e455a3b50ebf7c
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2022-08-27 14:53:49 +0500

    refactor: remove webVitals

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+	<React.StrictMode>
 		<BrowserRouter>
-    	<App />
+			<App />
 		</BrowserRouter>
-  </React.StrictMode>
+	</React.StrictMode>
 );
-
