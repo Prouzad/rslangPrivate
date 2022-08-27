5b14475a4d18f81cceecd022f4effe18e59bf76d
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2022-08-27 15:25:13 +0500

    fix: add modal windows open/close logic

--- src/components/Authorization/LoginPage.tsx
+++ src/components/Authorization/LoginPage.tsx
@@ -1,31 +1,21 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
-import LogIn from './LogIn/index'
+import LogIn from './LogIn/index';
 import { authStyle } from './AuthStyle';
 
-
 const LoginPage = () => {
-  const [openLogin, setOpenLogin] = React.useState(false);
-  const handleOpenLogin = () => setOpenLogin(true);
-  const handleCloseLogin = () => setOpenLogin(false);
-
-  return (
-    <div>
-      <Button sx={{ color: 'black' }} onClick={handleOpenLogin}>Временная кнопка LogIn</Button>
-      <Modal
-        open={openLogin}
-        onClose={handleCloseLogin}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={authStyle}>
-          <LogIn handleOpenLogin={handleOpenLogin} handleCloseLogin={handleCloseLogin} openLogin={openLogin} />
-        </Box>
-      </Modal>
-    </div>
-  );
-}
+	return (
+		<Modal
+			open={true}
+			aria-labelledby='modal-modal-title'
+			aria-describedby='modal-modal-description'
+		>
+			<Box sx={authStyle}>
+				<LogIn />
+			</Box>
+		</Modal>
+	);
+};
 
 export default LoginPage;
