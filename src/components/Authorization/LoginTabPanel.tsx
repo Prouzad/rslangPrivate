3ba2204e83c719ed740cc463f74248f4c68d692f
Seid Akhmed Agitaev <a.agitaev@gmail.com>
2022-08-27 22:05:51 +0500

    chore: typo on sign up

--- src/components/Authorization/LoginTabPanel.tsx
+++ src/components/Authorization/LoginTabPanel.tsx
@@ -8,59 +8,64 @@ import SignUp from './SignUp/index';
 import { authStyle } from './AuthStyle';
 
 interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
+	children?: React.ReactNode;
+	index: number;
+	value: number;
 }
 
 function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+	const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
+	return (
+		<div
+			role='tabpanel'
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
+	);
 }
 
 function a11yProps(index: number) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
+	return {
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
+	};
 }
 
 export default function LoginTabPanel() {
-  const [value, setValue] = React.useState(0);
+	const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
+	const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+		setValue(newValue);
+	};
 
-  return (
-    <Box sx={[{ width: '100%'}, authStyle]}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
-        <Tabs value={value} onChange={handleChange} centered aria-label="basic tabs example">
-          <Tab label="LogIn" {...a11yProps(0)} />
-          <Tab label="SighUp" {...a11yProps(1)} />
-        </Tabs>
-      </Box>
-      <TabPanel value={value} index={0}>
-        <LogIn />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <SignUp />
-      </TabPanel>
-    </Box>
-  );
+	return (
+		<Box sx={[{ width: '100%' }, authStyle]}>
+			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+				<Tabs
+					value={value}
+					onChange={handleChange}
+					centered
+					aria-label='basic tabs example'
+				>
+					<Tab label='LogIn' {...a11yProps(0)} />
+					<Tab label='Sign Up' {...a11yProps(1)} />
+				</Tabs>
+			</Box>
+			<TabPanel value={value} index={0}>
+				<LogIn />
+			</TabPanel>
+			<TabPanel value={value} index={1}>
+				<SignUp />
+			</TabPanel>
+		</Box>
+	);
 }
